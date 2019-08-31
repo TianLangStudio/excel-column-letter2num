510b381c6abb69d9aedf7a53ad5603fd9bf91e45
Tianlang <tianlangstuido@aliyun.com>
2019-08-31 12:42:00 +0800

    parseInt

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ exports.excelCol2Num = function (colName) {
   var letters = Array.from(colName.toUpperCase()),
         len = letters.length;
               return letters.reduce(function(start, ltr, idx){
-                return start + Math.pow(26, len - 1 - idx) * (ltr.charCodeAt(0) - 64);
+                return start + Math.pow(26, len - 1 - idx) * (parseInt(ltr, 36) - 9);
               }, 0);
 };
