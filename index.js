df0c4dcd1114f0a49af003484dd3e8d2b898d650
Tianlang <tianlangstuido@aliyun.com>
2019-08-31 12:48:03 +0800

    reverse

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 exports.excelCol2Num = function (colName) {
-  var letters = colName.toUpperCase().split(''),
-        len = letters.length;
-              return letters.reduce(function(start, ltr, idx){
-                return start + Math.pow(26, len - 1 - idx) * (parseInt(ltr, 36) - 9);
+      return  colName.toUpperCase().split('').reverse().reduce(function(start, ltr, idx){
+                return start + Math.pow(26, idx) * (parseInt(ltr, 36) - 9);
               }, 0);
 };
