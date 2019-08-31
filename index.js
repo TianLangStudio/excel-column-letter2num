b17a684e14fb3d7309d9915bceaa730502a949b7
Tianlang <tianlangstuido@aliyun.com>
2019-08-31 12:44:15 +0800

    split

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.excelCol2Num = function (colName) {
-  var letters = Array.from(colName.toUpperCase()),
+  var letters = colName.toUpperCase().split(''),
         len = letters.length;
               return letters.reduce(function(start, ltr, idx){
                 return start + Math.pow(26, len - 1 - idx) * (parseInt(ltr, 36) - 9);
