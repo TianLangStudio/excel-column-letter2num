b39e6e4950f0f702fca334a2202c9e9cd70ad9e0
Tianlang <tianlangstuido@aliyun.com>
2019-08-31 12:52:32 +0800

    charat

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.excelCol2Num = function (colName) {
       return  colName.toUpperCase().split('').reverse().reduce(function(start, ltr, idx){
-                return start + Math.pow(26, idx) * (parseInt(ltr, 36) - 9);
+                return start + Math.pow(26, idx) * (ltr.charCodeAt(0) - 64);
               }, 0);
 };
